5ae8cd5ab11790d9b227355045e6ac80a8a98f29
Samayun Chowdhury <samayun.m.chowdhury.com>
2021-04-26 04:32:19 +0600

    DevBook Boilarplate Code Started

---
+++ doc/convention.md
@@ -0,0 +1,7 @@
+## Conventions Developer must follow
+
+- Route Page er name sobsomoy `src/pages/route.page.jsx` erokom hobe ar tar component name hobe `RoutePage` er moto.
+- page e zodi external compoent lage `src/components/ComponentName.component.jsx` erokom namer hobe .
+  CapitalCase Hobe
+- routes list e add korar jonno ki korte hobe mathay kaj korche na pore bolbo neh :smile:
+- lazy loading use kora valo [optional]
--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./pages/loading.page";
+import routes from "./routes/routes";
+import Navbar from './layout/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Suspense fallback={<Loading />}>
+                <Navbar />
+                <Switch>
+                    {routes.map(route => {
+                        return (
+                            <Route
+                                path={route.path}
+                                exact={route.exact}
+                                component={route.component}
+                            />
+                        );
+                    })}
+                </Switch>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
