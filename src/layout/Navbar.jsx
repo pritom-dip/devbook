f3c2476adace8d22c6f6a725977413f8b86bf6c3
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-04-26 21:51:08 +0600

    DevBook

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
 import NavSearchPart from "./NavSearchPart.component";
 import NavbarMiddlePart from "./NavbarMiddlePart.component";
 import NavbarLastPart from "./NavbarLastPart.component";
@@ -26,17 +24,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-=======
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export default function Navbar() {
-    return (
-        <div>
-            <Link to="/"> Home </Link>
-            <Link to="/profile"> PRofile </Link>
-            <Link to="/dashboard"> dashboard </Link>
-        </div>
-    )
-}
->>>>>>> 1a672b776869de1e418b6fdb3fbbae071ca27680
