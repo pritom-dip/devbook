05e01ea7c68ed350b3375b9c1a14f9b08063413d
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-04-28 19:48:44 +0600

    change

---
+++ src/components/LoginComponents/LoginMainPart.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import LoginForm from "./LoginForm/LoginForm";
+import "./LoginMainPart.css";
+import Modal from "react-modal";
+import CreatNewAccountForm from "./RegisterForm/CreatNewAccountForm";
+
+const customStyles = {
+	content: {
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+		marginRight: "-50%",
+		marginTop: "40px",
+		transform: "translate(-50%, -50%)",
+	},
+};
+
+Modal.setAppElement("#root");
+
+const LoginMainPart = () => {
+	const [modalIsOpen, setIsOpen] = React.useState(false);
+	function openModal() {
+		setIsOpen(true);
+	}
+
+	function closeModal() {
+		setIsOpen(false);
+	}
+	return (
+		<div className="LoginMainPart d-flex justify-content-center align-items-center ">
+			<div className="row w-75 ">
+				<div className="col-7 pt-5">
+					<>
+						<h1>devbook</h1>
+						<h3>
+							Davbook helps you connect and
+							share with the people in your
+							life.
+						</h3>{" "}
+					</>
+				</div>
+				<div className="col-5 main-login-colum">
+					<LoginForm />
+					<hr className="specialLine" />
+					<div className="w-50 ml-auto mr-auto mt-3">
+						<Button
+							variant="info"
+							className="p-3"
+							onClick={openModal}
+						>
+							Create New Account
+						</Button>
+
+						<Modal
+							isOpen={modalIsOpen}
+							onRequestClose={closeModal}
+							style={customStyles}
+							contentLabel="Example Modal"
+						>
+							<CreatNewAccountForm />
+						</Modal>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LoginMainPart;
